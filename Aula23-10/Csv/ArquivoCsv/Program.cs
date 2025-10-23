14ff88e3188f12d56294ca8ff41c8e02928c8b4e
AneliseF <aneliseferreiralves12@gmail.com>
2025-10-23 20:01:35 -0300

    Add files via upload

---
+++ Aula23-10/Csv/ArquivoCsv/Program.cs
@@ -0,0 +1,38 @@
+﻿void Executar()
+{
+    string arq = @"C:\Users\2025106346\Documents\arquivo.csv";
+    string[] linhas = LerArquivo(arq);
+
+    foreach(string linha in linhas)
+    {
+        //Console.WriteLine(linha);
+        string[] colunas = linha.Split(';');
+        //irá retornar as colunas
+
+        //utilizando FOR mostrar colunas
+
+        for(int i = 0;i < colunas.Length; i++)
+        {
+            for(int a = 0;a < colunas[i].Length; a++)
+            {
+                
+            }
+        }
+    }
+}
+
+
+string[] LerArquivo(string arquivo)
+{
+    try
+    {
+        string[] linhas = File.ReadAllLines(arquivo);
+        return linhas;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro ao ler arquivo| {ex.ToString()}");
+        return new string[] { };
+    }
+}
+Executar();
