900eeb449123ff409ab2a42a75f403b94937ab56
AneliseF <aneliseferreiralves12@gmail.com>
2025-10-23 20:10:20 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿void Executar()
+{
+    string arq = @"C:\Users\2025106346\Documents\arquivo.csv";
+    string[] linhas = LerArquivo(arq);
+
+    foreach(string linha in linhas)
+    {
+        //Console.WriteLine(linha);
+        string[] colunas = linha.Split(';');
+        //irá retornar as colunas
+
+        //utilizando FOR mostrar colunas
+
+        for(int i = 0;i < colunas.Length; i++)
+        {
+            Console.Write($"{colunas[i].PadRight(10)}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+string[] LerArquivo(string arquivo)
+{
+    try
+    {
+        string[] linhas = File.ReadAllLines(arquivo);
+        return linhas;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro ao ler arquivo| {ex.ToString()}");
+        return new string[] { };
+    }
+}
+Executar();
